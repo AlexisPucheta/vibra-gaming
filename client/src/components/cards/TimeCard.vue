c8d28f2af30aa578b94348f166e8c4e6e5fb3875
Alexis Pucheta <alexispucheta22@gmail.com>
2023-09-01 14:03:43 -0300

    client created with vite. tailwind added. Card markup added.

---
+++ client/src/components/cards/TimeCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-52 p-2 rounded-xl border-black border-2 text-3xl bg-[#345678] m-2">
+    <div class="w-full flex justify-end">
+      <img src="../../assets/close.svg" class="h-4 w-4" alt="close icon" />
+      </div>
+    <div class="flex flex-col justify-center items-center h-36">
+      <p class="line-clamp-1">Europe/Rome</p>
+      <p>11/06/2020</p>
+      <p>5:35PM</p>
+    </div>
+  </div>
+</template>
---
+++ client/src/components/cards/TimeList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TimeCard from "./TimeCard.vue"
+</script>
+
+<template>
+    <div class="flex flex-col items-center sm:flex-row sm:flex-wrap sm:justify-start">
+        <TimeCard/>
+        <TimeCard/>
+        <TimeCard/>
+        <TimeCard/>
+        <TimeCard/>
+    </div>
+</template>
